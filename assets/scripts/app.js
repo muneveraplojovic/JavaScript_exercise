ee1e8f7b3cf9c4e080cc2a2d8ff9eaafca0a49a1
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-20 00:09:18 +0200

    events_listener_added_to_operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,23 +5,38 @@ function getUserInputNumber() {
   return parseInt(userInput.value);
 }
 
-function createAndWriteOutput(operator, resultBeforeCalc, CalcNumber) {
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${currentResult} ${operator} ${enteredNumber}`;
   outputResult(currentResult, calcDescription);
 }
+
 function add() {
   const enteredNumber = getUserInputNumber();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteOutput("+", currentResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = getUserInputNumber();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   createAndWriteOutput("-", currentResult, enteredNumber);
 }
-function multiply() {}
-function divide() {}
+
+function multiply() {
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", currentResult, enteredNumber);
+}
+
+function divide() {
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", currentResult, enteredNumber);
+}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
