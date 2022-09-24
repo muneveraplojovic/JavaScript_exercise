bb2e507467fbf5caeb454adaefd1417fa1a674a1
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-24 03:53:56 +0200

    counter

---
+++ counter/counter/script.js
@@ -0,0 +1,40 @@
+const decrease = document.getElementById("decrease")
+const increase = document.getElementById("increase")
+const reset = document.getElementById("reset")
+// const buttons = document.querySelectorAll("#button");
+let counter = document.getElementById("counter");
+
+let count = 0;
+
+increase.addEventListener('click', ()=>{
+    count++
+    counter.innerHTML = count
+ })
+
+ decrease.addEventListener('click', ()=>{
+    count--
+    counter.innerHTML = count
+ })
+
+ reset.addEventListener('click', ()=>{
+    count = 0
+    counter.innerHTML = count
+ })
+
+    
+    // buttons.forEach((b) => {
+    //   b.addEventListener("click", (e) => {
+    //     let target = e.currentTarget.classList;
+    
+    //     if (target.innerText == "increase") {
+    //       count++;
+    //       counter.innerHTML = count;
+    //     } else if (target.innerText == "decrease") {
+    //       count--;
+    //       counter.innerHTML = count;
+    //     } else if (target.innerText == "reset") {
+    //       count = 0;
+    //       counter.innerHTML = count;
+    //     }
+    //   });
+    // });
