478f072dec7a9a3937a4556e9674cdac0bccbe2c
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-25 01:53:23 +0200

    stopwatch

---
+++ stopwatch/stopwatch/script.js
@@ -0,0 +1,6 @@
+const timeBtns = document.getElementById("time");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+l
