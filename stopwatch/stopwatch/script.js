6094423ee8fe5f9403abe34695999db56ba5972f
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-25 20:01:37 +0200

    small_bag_fixed

--- stopwatch/stopwatch/script.js
+++ stopwatch/stopwatch/script.js
@@ -13,15 +13,24 @@ const stopWatch = function () {
   let minutes = Math.floor((sec - hours * 3600) / 60);
   let seconds = sec % 60;
 
+  seconds < 10 ? (seconds = `0${seconds}`) : seconds;
+  minutes < 10 ? (minutes = `0${minutes}`) : minutes;
+  hours < 10 ? (hours = `0${hours}`) : hours;
+
   timeBtns.innerHTML = `${hours}:${minutes}:${seconds}`;
 };
 
 const start = function () {
+  if (interval) {
+    return;
+  }
+  //uz pomoc if statement-a sam fiksala bag
   interval = setInterval(stopWatch, 1000);
   //interval postavljamo u varijablu da bismo ga mogli zaustaviti
 };
 const stop = function () {
   clearInterval(interval);
+  interval = null;
 };
 const reset = function () {
   stop();
