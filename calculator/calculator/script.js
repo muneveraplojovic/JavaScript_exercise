900d4877a10cb5a19e27300677b1b45a13c26bea
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-30 23:51:47 +0200

    ...

--- calculator/calculator/script.js
+++ calculator/calculator/script.js
@@ -1,6 +1,7 @@
 function display(){
     const outputValue = document.getElementById("output-value").innerHTML("001474")
     console.log(outputValue);
+    console.log("Hello world!!")
 }
 
 display()
