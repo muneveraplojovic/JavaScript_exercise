d28227133bf8610a9805c25b5afaf4c95174f6d4
Munevera Plojovic <plojovicm3@gmail.com>
2022-10-10 01:18:26 +0200

    counter

---
+++ counter2/script.js
@@ -0,0 +1,24 @@
+
+//elements
+let displayNumber = document.querySelector(".display_number");
+let increase = document.querySelector(".increase");
+let decrease = document.querySelector(".decrease");
+let reset = document.querySelector(".reset");
+
+let count = 0;
+
+//event listeners and arrow functions
+increase.addEventListener("click", () => {
+  count++;
+  displayNumber.innerHTML = count;
+});
+
+decrease.addEventListener("click", () => {
+  count--;
+  displayNumber.innerHTML = count;
+});
+
+reset.addEventListener("click", () => {
+  count = 0;
+  displayNumber.innerHTML = count;
+});
