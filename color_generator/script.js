9cc0ebbfe3b550427a9e5e2dff19c1b07557392b
Munevera Plojovic <plojovicm3@gmail.com>
2022-10-16 01:18:50 +0200

    color_generator_func

--- color_generator/script.js
+++ color_generator/script.js
@@ -4,12 +4,14 @@ let colorCode = document.querySelector(".color_code");
 let colorGenerator = document.querySelector(".color_generator_btn");
 
 //functions
-function randomColor(){
-    let randomColor = "#"+ Math.random().toString(16).substring(2,8)
-    randomBackgorund.style.background=randomColor
-    colorCode.innerHTML=randomColor
+function randomColor() {
+  let randomColor = "#" + Math.random().toString(16).substring(2, 8);
+  randomBackgorund.style.background = randomColor;
+  colorCode.innerHTML = randomColor;
 }
-randomColor()
+randomColor();
+
+//pozivamo funkciju kako bi se menjala i pri svakom refreshovanju stranice
 //event listeners
 
-colorGenerator.addEventListener("click", randomColor)
+colorGenerator.addEventListener("click", randomColor);
