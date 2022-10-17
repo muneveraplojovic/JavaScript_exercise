d601a0672462ba6ee7736cc0676cecc634e70159
Munevera Plojovic <plojovicm3@gmail.com>
2022-10-18 00:28:02 +0200

    ...

---
+++ bmi_calculator/script.js
@@ -0,0 +1,24 @@
+// const secondTask = (arr) => {
+//     let duzina = arr.length;
+//     for (let i = 0; i <= duzina; i++) {
+//       if (i % 2 === 1) {
+//         arr[i] = "paran broj";
+//       }
+//     }
+//     return arr;
+//   };
+
+//   console.log(secondTask([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]));
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+const removeEvenNumber = () => {
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 == 1) {
+      array[i] = "paran broj";
+    }
+  }
+  return array;
+};
+
+console.log(removeEvenNumber());
