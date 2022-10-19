5e2a29f53c2f44e21c8655305f31ccc50c204217
Munevera Plojovic <plojovicm3@gmail.com>
2022-10-19 22:34:28 +0200

    exercise

--- bmi_calculator/script.js
+++ bmi_calculator/script.js
@@ -10,15 +10,29 @@
 
 //   console.log(secondTask([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]));
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-const removeEvenNumber = () => {
-  for (let i = 0; i < array.length; i++) {
-    if (i % 2 == 1) {
-      array[i] = "paran broj";
-    }
-  }
-  return array;
-};
+// const removeEvenNumber = () => {
+//   for (let i = 0; i < array.length; i++) {
+//     if (i % 2 == 1) {
+//       array[i] = "paran broj";
+//     }
+//   }
+//   return array;
+// };
 
-console.log(removeEvenNumber());
+// console.log(removeEvenNumber());
+
+
+let paragraf = document.getElementById("some-text")
+let button=document.getElementById("btn")
+let again =document.getElementById("back")
+
+button.addEventListener("click", ()=>{
+  paragraf.innerHTML="This paragraph is changed !"
+})
+
+again.addEventListener("click", ()=>{
+  window.location.reload();
+
+})
