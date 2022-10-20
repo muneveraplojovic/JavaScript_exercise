69642292b26fb0c25728f71c9b282b7aa4aee62a
Munevera Plojovic <plojovicm3@gmail.com>
2022-10-20 02:54:52 +0200

    bmi_calc

--- bmi_calculator/script.js
+++ bmi_calculator/script.js
@@ -60,38 +60,71 @@
 //   brojac.innerHTML=counter
 // })
 
-let modalShow=document.getElementById("display_modal")
-let prviDiv = document.querySelector(".first_page")
-let confirmBtn=document.getElementById("confirm")
-let drugiDiv=document.querySelector(".second_page")
-let exitBtnSecond=document.getElementById("exit2")
-let confirmBtnSecond = document.getElementById("confirm2")
-let lastPage= document.querySelector(".last_page")
-let backBtn = document.getElementById("get_back")
+// let modalShow=document.getElementById("display_modal")
+// let prviDiv = document.querySelector(".first_page")
+// let confirmBtn=document.getElementById("confirm")
+// let drugiDiv=document.querySelector(".second_page")
+// let exitBtnSecond=document.getElementById("exit2")
+// let confirmBtnSecond = document.getElementById("confirm2")
+// let lastPage= document.querySelector(".last_page")
+// let backBtn = document.getElementById("get_back")
 
 
-modalShow.addEventListener("click", ()=>{
-  prviDiv.style.display="unset"
-  modalShow.style.display="none"
-})
+// modalShow.addEventListener("click", ()=>{
+//   prviDiv.style.display="unset"
+//   modalShow.style.display="none"
+// })
 
-confirmBtn.addEventListener("click", ()=>{
-  prviDiv.style.display="none"
-  drugiDiv.style.display="unset"
-})
+// confirmBtn.addEventListener("click", ()=>{
+//   prviDiv.style.display="none"
+//   drugiDiv.style.display="unset"
+// })
 
-exitBtnSecond.addEventListener("click", ()=>{
-  drugiDiv.style.display="none"
-  prviDiv.style.display="unset"
-})
+// exitBtnSecond.addEventListener("click", ()=>{
+//   drugiDiv.style.display="none"
+//   prviDiv.style.display="unset"
+// })
 
-confirmBtnSecond.addEventListener("click", ()=>{
-  drugiDiv.style.display="none"
-  lastPage.style.display="unset"
-})
+// confirmBtnSecond.addEventListener("click", ()=>{
+//   drugiDiv.style.display="none"
+//   lastPage.style.display="unset"
+// })
 
-backBtn.addEventListener("click", ()=>{
-  window.location.reload();
+// backBtn.addEventListener("click", ()=>{
+//   window.location.reload();
 
-})
+// })
 
+// Elements
+let btnCalculate = document.querySelector('.calculate');
+let btnReset = document.querySelector('.reset');
+let outputMessage = document.querySelector('.result-message');
+
+// Functions
+function calculateBMI(event) {
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+    event.preventDefault();
+
+    let bmi = weight / (height / 100 * height / 100);
+    const finalBMI = (bmi.toFixed(2));
+
+    if (height === '' || weight === '') {
+        outputMessage.innerHTML = 'Enter a valid data.';
+    } else if (finalBMI <= 18.5) {
+        outputMessage.innerHTML = `Your BMI is ${finalBMI}. You are thin.`
+    } else if (finalBMI > 18.5 && finalBMI < 24.9) {
+        outputMessage.innerHTML = `Your BMI is ${finalBMI}. You are healthy.`
+    } else {
+        outputMessage.innerHTML = `Your BMI is ${finalBMI}. You are overweight.`
+    }
+}
+
+function reloadPage() {
+    window.location.reload();
+}
+
+// Event listeners
+btnCalculate.addEventListener('click', calculateBMI);
+
+btnReset.addEventListener('click', reloadPage);
