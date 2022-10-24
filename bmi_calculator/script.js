2c1832d78cd26b93f59db8be4bf4146ce48c9902
Munevera Plojovic <plojovicm3@gmail.com>
2022-10-24 23:57:53 +0200

    bmi_calc

--- bmi_calculator/script.js
+++ bmi_calculator/script.js
@@ -129,3 +129,12 @@
 
 // btnReset.addEventListener('click', reloadPage);
 
+// import React from 'react'
+
+// function script() {
+//   return (
+//     <div>script</div>
+//   )
+// }
+
+// export default script
