49762bd6c04b81f5c5249d7647f5f0fdd53ee551
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-01 17:20:41 +0100

    folder for exercise and testing

---
+++ testing_exercises/script.js
@@ -0,0 +1,9 @@
+let button=document.getElementById("btn")
+
+
+
+button.addEventListener("click", ()=>{
+    document.getElementById("header").innerHTML= "Ovo je sad novi header"
+    console.log("stisnuli ste dugmee");
+    alert("radiiiiii")
+})  
