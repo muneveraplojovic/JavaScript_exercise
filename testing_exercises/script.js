2a8619f22ac178c8c99366f1e6ff780e2830c4ba
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-06 14:39:27 +0100

    exercises

--- testing_exercises/script.js
+++ testing_exercises/script.js
@@ -1,30 +1,29 @@
-// let button=document.getElementById("btn")
+let button=document.getElementById("btn")
 
-// button.addEventListener("click", ()=>{
-//     document.getElementById("header").innerHTML= "Ovo je sad novi header"
-//     console.log("stisnuli ste dugmee");
-//     alert("radiiiiii")
-// })
+button.addEventListener("click", ()=>{
+    document.getElementById("header").innerHTML= "Ovo je sad novi header"
+    console.log("stisnuli ste dugmee");
+    alert("radiiiiii")
+})
 
-// const string = "ovo je string";
-// let newString = "";
+const string = "ovo je string";
+let newString = "";
 
-// for (let i = 0; i <string.length; i++) {
-//   if (i === string.length - 1) {
-//     newString += string[i].toUpperCase();
-//   } else if (string[i + 1] === " ") {
-//     newString += string[i].toUpperCase();
-//   } else {
-//     newString += string[i];
-//   }
-// }
+for (let i = 0; i <string.length; i++) {
+  if (i === string.length - 1) {
+    newString += string[i].toUpperCase();
+  } else if (string[i + 1] === " ") {
+    newString += string[i].toUpperCase();
+  } else {
+    newString += string[i];
+  }
+}
 
-// console.log(newString);
+console.log(newString);
 
-// let weatherIsSunny = false
+let weatherIsSunny = false
 
-// weatherIsSunny ? console.log("ponesi naocare") : console.log("sedi kuci hladno je");;
-
+weatherIsSunny ? console.log("ponesi naocare") : console.log("sedi kuci hladno je");;
 
 // Zadatak
 // Prvi deo. Napraviti pomocni niz koji ce sadrzati elemente od oba niza.
@@ -32,93 +31,90 @@
 // Treci deo. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
 //            Funkcija na kraju treba da vrati dva napravljena pomocna niza.
 
-// const arr1 = [2, 26, 38, 75, 11, 29];
-// const arr2 = ["a", "b", "c", "d", "e"];
+const arr1 = [2, 26, 38, 75, 11, 29];
+const arr2 = ["a", "b", "c", "d", "e"];
 
-// function concatArrays(a, b) {
-//   concatedArray = [];
-//   let conc = a.concat(b)
-//   concatedArray.push(conc);
+function concatArrays(a, b) {
+  concatedArray = [];
+  let conc = a.concat(b)
+  concatedArray.push(conc);
 
-//   let pomocniArr = [];
-//   for (let i = 0; i <= a.length; i++) {
-//     if (i % 2 === 0) {
-//       pomocniArr.push(i);
-//     }
-//   }
-//   pomocniArr.unshift(10, 20);
-//   console.log("Prvi array:  ", concatedArray);
-//   console.log("Drugi array:  ", pomocniArr);
-// }
-// console.log(concatArrays(arr1, arr2));
+  let pomocniArr = [];
+  for (let i = 0; i <= a.length; i++) {
+    if (i % 2 === 0) {
+      pomocniArr.push(i);
+    }
+  }
+  pomocniArr.unshift(10, 20);
+  console.log("Prvi array:  ", concatedArray);
+  console.log("Drugi array:  ", pomocniArr);
+}
+console.log(concatArrays(arr1, arr2));
 
-// console.log("------------------------------------");
+console.log("------------------------------------");
 
-// const DATA1 = [2, 26, 38, 75, 11, 29];
-// const DATA2 = ["a", "b", "c", "d", "e"];
+const DATA1 = [2, 26, 38, 75, 11, 29];
+const DATA2 = ["a", "b", "c", "d", "e"];
 
-// const mutateArrays = (arr1, arr2) => {
-//   const joker1 = arr1.concat(arr2);
-//   let joker2 = [];
-//   for (let i of joker1) {
-//     if (i % 2 === 0) {
-//       joker2.push(i);
-//     }
-//   }
-//   joker2.unshift(10, 20);
-//   return (
-//     "Prvi pomocni niz je: " + joker1 + "\n" + "Drugi pomocni niz je " + joker2
-//   );
-// };
+const mutateArrays = (arr1, arr2) => {
+  const joker1 = arr1.concat(arr2);
+  let joker2 = [];
+  for (let i of joker1) {
+    if (i % 2 === 0) {
+      joker2.push(i);
+    }
+  }
+  joker2.unshift(10, 20);
+  return (
+    "Prvi pomocni niz je: " + joker1 + "\n" + "Drugi pomocni niz je " + joker2
+  );
+};
 
-// console.log(mutateArrays(DATA1, DATA2));
+console.log(mutateArrays(DATA1, DATA2));
 
+const DATA8 = ["Bostan", "bostan", 39, "grozdje", 10, "borovnica"];
 
-// const DATA8 = ["Bostan", "bostan", 39, "grozdje", 10, "borovnica"];
+const toString = DATA8.join("");
+console.log(toString);
 
-// const toString = DATA8.join("");
-// console.log(toString);
+// Napraviti novi niz koji ce sadrzati samo ocene. Nakon toga,
+//  Naci prosek ocena.
 
+const students = [
+    { name: "Faris", grade: 8, year: 2 },
+    { name: "Isko", grade: 10, year: 4 },
+    { name: "Aldin", grade: 8, year: 2 },
+    { name: "Amina", grade: 9, year: 4 },
+    { name: "Sabina", grade: 9, year: 3 },
+    { name: "Denis", grade: 7, year: 4 },
+    { name: "Senad", grade: 6, year: 1 },
+]
 
-//  Napraviti novi niz koji ce sadrzati samo ocene. Nakon toga,
-// //  Naci prosek ocena.
+function getAvgGrade(array) {
+    const grades = array.map((value) => value.grade);
+    let len = grades.length;
+    const avg = grades.reduce((total, value) => total + value) / len;
+    return +(avg.toFixed(2));
+}
 
-// const students = [
-//     { name: "Faris", grade: 8, year: 2 },
-//     { name: "Isko", grade: 10, year: 4 },
-//     { name: "Aldin", grade: 8, year: 2 },
-//     { name: "Amina", grade: 9, year: 4 },
-//     { name: "Sabina", grade: 9, year: 3 },
-//     { name: "Denis", grade: 7, year: 4 },
-//     { name: "Senad", grade: 6, year: 1 },
-// ]
-
-// function getAvgGrade(array) {
-//     const grades = array.map((value) => value.grade);
-//     let len = grades.length;
-//     const avg = grades.reduce((total, value) => total + value) / len;
-//     return +(avg.toFixed(2));
-// }
-
-
-//push - dodaje element na kraju niza
-//pop - izbacuje zadnji element - brise ga
-//unshift - dodaje el na pocetak niza
-//shift - brise prvi element
-//reverse()-vraca obrnuti redosledd niza
-//concat - sluzi za spajanje
-//find( )- vraca prvi el iz niza koji zadovoljava uslov
-//findIndex()- vraca prvi el koji ima trazeni index
-//filter - prvi novi niz na osnovu ispunjenog uslova
-//map - pravi novi niz uz dodavanje promena
+// push - dodaje element na kraju niza
+// pop - izbacuje zadnji element - brise ga
+// unshift - dodaje el na pocetak niza
+// shift - brise prvi element
+// reverse()-vraca obrnuti redosledd niza
+// concat - sluzi za spajanje
+// find( )- vraca prvi el iz niza koji zadovoljava uslov
+// findIndex()- vraca prvi el koji ima trazeni index
+// filter - prvi novi niz na osnovu ispunjenog uslova
+// map - pravi novi niz uz dodavanje promena
 //  every() metoda. Da li svaki element niza ispunjava odredjeni uslov?!
 //  some() metoda. Da li je bar neki element niza ispunio odredjeni uslov?!
 //  includes() metoda. Da li dati niz sadrzi odredjeni element?!
 //  join() metoda. Uzima sve clanove niza i spaja ih u jedan string.
 // console.log(getAvgGrade(students));
-//call , bind apply
+// call , bind apply
 // Object.freeze() cini objekat immutabile(nepromenljivim)!!!
-// Object.seal() cini objekat promenljivim, ali nije dozvoljeno 
+// Object.seal() cini objekat promenljivim, ali nije dozvoljeno
 // dodati niti brisati elemente!!!
 // Object.keys() vraca niz kljuceva!!!
 // Object.values() vraca niz vrednosti objekta!!!
@@ -129,75 +125,72 @@
 // const nemaDuplih = new Set(numbers);
 // const nemaDuplihArr = [...nemaDuplih];
 // console.log(nemaDuplihArr);
-//Promise - resolve/reject,   then-uspesno povuceni podaci/catch(err)
-//clousure, block local global scope
-//fetch-async/await , json
+// Promise - resolve/reject,   then-uspesno povuceni podaci/catch(err)
+// clousure, block local global scope
+// fetch-async/await , json
 
+const person1 = {
+    name: "John Doe",
+    age: 48,
+    job: "Employed",
+    address: "USA",
+    kids: 4
+};
 
+const person2 = person1;
+Object.freeze(person1);
+Object.seal(person1);
+Object.keys(person1);
+Object.values(person1);
+Object.entries(person1)
+Object.is(person1, person2);
+console.log(Object.is(person1, person2));
 
-// const person1 = {
-//     name: "John Doe",
-//     age: 48,
-//     job: "Employed",
-//     address: "USA",
-//     kids: 4
-// };
+const naames = ["faris", "aldin", "senad", "dzenan", "sabina"];
+const newNames = ["isko", ...names, "hamza"] // pravi novi niz sa istim elementima.
+console.log(names);
+console.log(newNames);
 
-// const person2 = person1;
-// Object.freeze(person1);
-// Object.seal(person1);
-// Object.keys(person1);
-// Object.values(person1);
-// Object.entries(person1)
-// Object.is(person1, person2);
-// console.log(Object.is(person1, person2));
+const numbers = [1, 2, 3, 1, 5, 2, 7, 10];
 
-// const names = ["faris", "aldin", "senad", "dzenan", "sabina"];
-// const newNames = ["isko", ...names, "hamza"] // pravi novi niz sa istim elementima.
-// console.log(names);
-// console.log(newNames);
+// Preko new Set() gubimo duplikate, ali je potrebno opet vratiti u niz.
 
-// const numbers = [1, 2, 3, 1, 5, 2, 7, 10];
+const nemaDuplih = new Set(numbers);
+const nemaDuplihArr = [...nemaDuplih];
+console.log(nemaDuplihArr);
 
-// // Preko new Set() gubimo duplikate, ali je potrebno opet vratiti u niz.
+// spread moze takodje spajati nizove
 
-// const nemaDuplih = new Set(numbers);
-// const nemaDuplihArr = [...nemaDuplih];
-// console.log(nemaDuplihArr);
+const names = ["faris", "aldin", "senad", "dzenan", "sabina"];
 
-// // spread moze takodje spajati nizove
+const spojeni = [...numbers, ...names];
 
-// const names = ["faris", "aldin", "senad", "dzenan", "sabina"];
+console.log(spojeni);
 
-// const spojeni = [...numbers, ...names];
+1
+n= Number(prompt("Unesite neki broj:  "))
+let square;
 
-// console.log(spojeni);
+if (isNaN(n)){
+    console.log("Molimo vas unesite broj!!!");
+} else{
+    square=n*n
+    console.log(square);
+}
 
+2
 
-//1
-// n= Number(prompt("Unesite neki broj:  "))
-// let square;
-
-// if (isNaN(n)){
-//     console.log("Molimo vas unesite broj!!!");
-// } else{
-//     square=n*n
-//     console.log(square);
-// }
-
-//2
-
-// for (let i = 1; i <=100; i++) {
-//     if(i%3==0 && i%5==0){
-//         console.log("fizz-buzz");
-//     }else if(i%3==0){
-//         console.log("fizz");
-//     }else if(i%5==0){
-//         console.log("buzz");
-//     }else{
-//         console.log(i);
-//     }
-// }
+for (let i = 1; i <=100; i++) {
+    if(i%3==0 && i%5==0){
+        console.log("fizz-buzz");
+    }else if(i%3==0){
+        console.log("fizz");
+    }else if(i%5==0){
+        console.log("buzz");
+    }else{
+        console.log(i);
+    }
+}
 
 //3
 
@@ -214,3 +207,5 @@ for (let i = 0; i <recenica.length; i++) {
     }
 }
     console.log(novaRecenica);
+
+// -------------------------------------------------------------------------
