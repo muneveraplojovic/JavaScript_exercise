f2b7f25c59c371ddf03d8fe01bcbc5676d479227
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-10 18:53:04 +0100

    ///

--- js_course/script.js
+++ js_course/script.js
@@ -8,8 +8,8 @@
 // i--
 // }
 
-//rekurzija - pozivanje funkcije unutar iste
-//moze da se postigne efekat petlje
+// rekurzija - pozivanje funkcije unutar iste
+// moze da se postigne efekat petlje
 
 // function ispitivanje(n) {
 //   console.log(n);
@@ -22,8 +22,8 @@
 // }
 // ispitivanje(10);
 
-//callback funkcije - funkcija koja se nadje kao argument neke metode ili funkcije
-//(ovo unutra je parametar), argument je kao funkcija
+// callback funkcije - funkcija koja se nadje kao argument neke metode ili funkcije
+// (ovo unutra je parametar), argument je kao funkcija
 
 // let talib = "Talib";
 
@@ -37,10 +37,10 @@
 // }
 
 // main(callBack);  ne pozivamo funkciju jer smo je prosledili samo kao argument
-//callback funkcijaaaaaaaaa
+// callback funkcijaaaaaaaaa
 
-//napraviti main funk koja ispisuje poruku da se raadi o glavnoj funk, 
-//ali ispod toga neka izvrsi pozivanje callback funk koja ima zadatak da izracuna aritmeticku sredinu 3 broja
+// napraviti main funk koja ispisuje poruku da se raadi o glavnoj funk,
+// ali ispod toga neka izvrsi pozivanje callback funk koja ima zadatak da izracuna aritmeticku sredinu 3 broja
 // koji predstavljaju argumente te callback funk
 
 // function main2(func) {
@@ -55,7 +55,34 @@
 
 // main2(callBack);
 
+// ---------------------------------------------------------
 
-//---------------------------------------------------------
+// Objekti su glavna stvar u js
+// key-value
+
+// const car = {
+//   name: "fiat",
+//   godiste: 2004,
+//   boja: "bela",
+// };
+
+// console.log(car.name);
+// console.log(car["godiste"]);
+
+// obj takodje mogu sadrzati metode 
+// metode su sadrzane unutar objekta kao definicija funkcije
+
+// const person = {
+//     name: "Amina", 
+//     prezime: "N",
+//     jmbg: 1312003788917,
+//     fullName: function(){
+//         return this.name + " " + this.prezime
+//     }
+// }
+// console.log(person.fullName());
 
 
+// napraviti objekat student koji ce da ima vrednosti po zelji svojstava sledecih: 
+// ime, prezime, br indexa, ocene kao niz svih ocene koje student ima u trenutnoj godini, 
+// prosek ocena ( funkcija koja na osnovu niza ocena vraca prosek datih ocena) 
