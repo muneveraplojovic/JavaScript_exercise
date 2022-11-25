f4c26456910163796b5ec9afabfe316c8e5d8ff2
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-25 02:21:47 +0100

    modal

--- js_course/script.js
+++ js_course/script.js
@@ -275,3 +275,40 @@
 // const mapDispatchToProps = {}
 
 // export default connect(mapStateToProps, mapDispatchToProps)(script)
+
+
+//
+
+let leftMainBtn = document.querySelector(".btn-main-1st");
+let rightMainBtn = document.querySelector(".btn-main-2nd");
+let yesBtn = document.querySelector(".btn-modal-1st");
+let notBtn = document.querySelector(".not-yet");
+let exitBtn = document.querySelector(".exit-btn");
+let lastBtn = document.querySelector(".btn-modal-2nd");
+let firstModal = document.querySelector(".first-modal");
+let secondModal = document.querySelector(".second-modal");
+
+leftMainBtn.addEventListener("click", function(){
+    firstModal.classList.remove("first-modal")
+})
+
+rightMainBtn.addEventListener("click", function(){
+    firstModal.classList.remove("first-modal")
+})
+
+yesBtn.addEventListener("click", function(){
+    firstModal.classList.add("first-modal")
+    secondModal.classList.remove("second-modal")
+})
+
+notBtn.addEventListener("click", function(){
+    firstModal.classList.add("first-modal")
+})
+
+lastBtn.addEventListener("click", function(){
+    secondModal.classList.add("second-modal")
+})
+
+exitBtn.addEventListener("click", function(){
+    firstModal.classList.add("first-modal")
+})
