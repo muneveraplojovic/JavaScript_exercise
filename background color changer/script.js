28ac0c2e529d46afa8040bb984abb73f19c6972a
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-30 12:34:03 +0100

    added functionality-second attempt

--- background color changer/script.js
+++ background color changer/script.js
@@ -1,14 +1,17 @@
-const colorButton = document.querySelector(".colorBtn")
-const  body = document.querySelector(".body")
+//elements
+let randomBackgorund = document.querySelector(".background");
+let colorCode = document.querySelector(".color_code");
+let colorGenerator = document.querySelector(".color_generator_btn");
 
 //functions
 function randomColor() {
   let randomColor = "#" + Math.random().toString(16).substring(2, 8);
-  body.style.background = randomColor;
+  randomBackgorund.style.background = randomColor;
+  colorCode.innerHTML = randomColor;
 }
 randomColor();
 
 //pozivamo funkciju kako bi se menjala i pri svakom refreshovanju stranice
 //event listeners
 
-colorButton.addEventListener("click", randomColor);
+colorGenerator.addEventListener("click", randomColor);
