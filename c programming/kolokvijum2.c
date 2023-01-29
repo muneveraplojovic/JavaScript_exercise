953549b5cbaeb7cfc917226790f18b9e8eb89fb4
Munevera Plojovic <plojovicm3@gmail.com>
2023-01-30 00:04:09 +0100

    pov trougla

--- c programming/kolokvijum2.c
+++ c programming/kolokvijum2.c
@@ -23,43 +23,73 @@
 //#include <stdio.h>
 
 
-int ZbirKvadrata(int a, int b){
-    int i, suma=0;
-    for(i=a;i<=b;i++){
-        suma+=i*i;
+// int ZbirKvadrata(int a, int b){
+//     int i, suma=0;
+//     for(i=a;i<=b;i++){
+//         suma+=i*i;
         
-    return suma;    
-    }
+//     return suma;    
+//     }
+// }
+
+// int ZbirKvadrataParnih(int a, int b){
+//     int i, suma = 0;
+//     for(i=a;i<=b;i++){
+//         if(i%2==0){
+//             suma+=i*i;
+//         }
+//     return suma;
+//     }
+// }
+
+// int ZbirKvadrataNeparnih(int a, int b){
+//     int i, suma = 0;
+//     for(i=a;i<=b;i++){
+//         if(i%2==1){
+//             suma+=i*i;
+//         }
+//     return suma;
+//     }
+// }
+
+// int main(){
+//     int a,b;
+//      printf("Donja granica: ");
+//     scanf("%d", &a);
+//     printf("Gornja granica: ");
+//     scanf("%d", &b);
+//     printf("\nZbirKvadrata: %d", ZbirKvadrata(a,b));
+//     printf("\nZbirKvadrataParnih: %d", ZbirKvadrataParnih(a,b));
+//     printf("\nZbirKvadrataNeparnih: %d", ZbirKvadrataNeparnih(a,b));
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// #include <math.h>
+
+double Stranica(double x1, double x2, double y1, double y2){
+    return(sqrt(pow(x1-x2, 2) + pow(y1-y2,2 )));
 }
 
-int ZbirKvadrataParnih(int a, int b){
-    int i, suma = 0;
-    for(i=a;i<=b;i++){
-        if(i%2==0){
-            suma+=i*i;
-        }
-    return suma;
-    }
-}
 
-int ZbirKvadrataNeparnih(int a, int b){
-    int i, suma = 0;
-    for(i=a;i<=b;i++){
-        if(i%2==1){
-            suma+=i*i;
-        }
-    return suma;
-    }
-}
-
-int main(){
-    int a,b;
-     printf("Donja granica: ");
-    scanf("%d", &a);
-    printf("Gornja granica: ");
-    scanf("%d", &b);
-    printf("\nZbirKvadrata: %d", ZbirKvadrata(a,b));
-    printf("\nZbirKvadrataParnih: %d", ZbirKvadrataParnih(a,b));
-    printf("\nZbirKvadrataNeparnih: %d", ZbirKvadrataNeparnih(a,b));
+ main(){
+    double xa, xb, xc,ya,yb,yc,a,b,c,s,p;
+    
+    printf("xa, ya koordinate: \n"); 
+    scanf("%lf%lf", &xa,&ya);
+    
+    printf("xb, yb koordinate: \n");
+    scanf("%lf%lf", &xb,&yb);
+    
+    printf("xc, yc koordinate: \n");
+    scanf("%lf%lf", &xc,&yc);
+    
+    a=Stranica(xb,xc,yb,yc);
+    b=Stranica(xc,xa,yc,ya);
+    c=Stranica(xa,xb,ya,yb);
+    s=(a+b+c)/2;
+    p=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("\nPovrsina trougla iznosi: %.2f\n", p);
     return 0;
 }
