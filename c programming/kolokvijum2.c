bb8129663ea22120bfcbfe293f0996f5097f6e5f
Munevera Plojovic <plojovicm3@gmail.com>
2023-01-29 23:47:06 +0100

    funkcije

--- c programming/kolokvijum2.c
+++ c programming/kolokvijum2.c
@@ -1,20 +1,65 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int max(int a, int b){
-    if(a==b) return 0;
-    else if(a>b) return a;
-    else return b;
+// int max(int a, int b){
+//     if(a==b) return 0;
+//     else if(a>b) return a;
+//     else return b;
+// }
+
+// int main()
+// {
+//     int a,b,c,d,najveci;
+//     printf("Unesite 4 broja: \n");
+//     scanf("%d %d %d %d", &a, &b, &c, &d);
+//     najveci = max(max(a,b), max(c,d));
+//     if(najveci==0) printf("Brojevi su isti.\n");
+//     else printf("\nNajveci je: %d\n", najveci);
+
+
+//     return 0;
+// }
+
+
+//#include <stdio.h>
+
+
+int ZbirKvadrata(int a, int b){
+    int i, suma=0;
+    for(i=a;i<=b;i++){
+        suma+=i*i;
+        
+    return suma;    
+    }
 }
 
-int main()
-{
-    int a,b,c,d,najveci;
-    printf("Unesite 4 broja: \n");
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    najveci = max(max(a,b), max(c,d));
-    if(najveci==0) printf("Brojevi su isti.\n");
-    else printf("\nNajveci je: %d\n", najveci);
+int ZbirKvadrataParnih(int a, int b){
+    int i, suma = 0;
+    for(i=a;i<=b;i++){
+        if(i%2==0){
+            suma+=i*i;
+        }
+    return suma;
+    }
+}
 
+int ZbirKvadrataNeparnih(int a, int b){
+    int i, suma = 0;
+    for(i=a;i<=b;i++){
+        if(i%2==1){
+            suma+=i*i;
+        }
+    return suma;
+    }
+}
 
+int main(){
+    int a,b;
+     printf("Donja granica: ");
+    scanf("%d", &a);
+    printf("Gornja granica: ");
+    scanf("%d", &b);
+    printf("\nZbirKvadrata: %d", ZbirKvadrata(a,b));
+    printf("\nZbirKvadrataParnih: %d", ZbirKvadrataParnih(a,b));
+    printf("\nZbirKvadrataNeparnih: %d", ZbirKvadrataNeparnih(a,b));
     return 0;
 }
